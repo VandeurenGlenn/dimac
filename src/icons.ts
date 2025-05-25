b3ef2352a4e2236d2e869cf8595b4719f9fa8921
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-05-25 15:32:02 +0200

    Create icons.ts

---
+++ src/icons.ts
@@ -0,0 +1,12 @@
+import { html } from '@vandeurenglenn/lite'
+import '@vandeurenglenn/lite-elements/icon-set.js'
+
+export default html`
+  <custom-icon-set>
+    <template>
+      <span name="home">@symbol-home</span>
+      <span name="menu">@symbol-menu</span>
+      <span name="menu_open">@symbol-menu_open</span>
+    </template>
+  </custom-icon-set>
+`
