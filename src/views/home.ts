52b5fc6b315511e2f5e544706af629af67671c54
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-07-31 22:53:44 +0200

    Update typing animation strings on home view
    Replaces the strings used in the typing animation to better reflect the desired messaging. 'snel', 'betaalbaar', and 'slim' were removed, and 'smart' was added.

--- src/views/home.ts
+++ src/views/home.ts
@@ -28,7 +28,7 @@ export default customElements.define(
 
     startTyping() {
       var typed3 = new Typed(this.shadowRoot.querySelector('.typer'), {
-        strings: ['wij maken jouw droom', 'snel', 'betaalbaar', 'duurzaam', 'slim', 'met oog voor detail'],
+        strings: ['wij maken jouw droom', 'duurzaam', 'smart', 'met oog voor detail'],
         typeSpeed: 100,
         backSpeed: 25,
         cursorChar: '_',
